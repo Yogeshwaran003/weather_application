897cb76a16c51db739b99c3f6986de9248cebb89
Yogeshwaran B <yogiwar33@gmail.com>
2025-05-26 12:36:08 +0530

    Added the time Feature

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,22 +1,23 @@
 import React from 'react'
 
 const card = (props:{place:string;temp:number}) => {
+  const now=Date.now();
   return (
     <div className='flex flex-col justify-around h-[600px] w-[500px] p-5 bg-black bg-transparent backdrop-blur-[20px] shadow-[0_8px_32px_0_rgba(31,38,135,0.37)] rounded-[10px] border border-white/20  '>
       {/* <div>Weather application</div> */}
       <div className='p-1 flex justify-between'>
-        <p className='text-[25px]'>Date</p>
-        <p className='text-[25px]'>Time</p>
+        <p className='text-[25px]'>{new Date(now).toLocaleDateString()}</p>
+        <p className='text-[25px]'>{new Date(now).toLocaleTimeString()}</p>
       </div>
       <div className='flex justify-center text-[100px]'>
-        <h1>{Math.round(props.temp-273.15)}°C</h1>
+        <h1>{props.temp==0?0:Math.round(props.temp-273.15)}°C</h1>
       </div>
       <div className='flex justify-center text-[50px]'>
         {props.place}
       </div>
-      <div className='flex justify-center text-[50px]'>
+      {/* <div className='flex justify-center text-[50px]'>
         Some Extra Feature
-      </div>
+      </div> */}
     </div>
   )
 }
