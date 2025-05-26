9bb2095665e2805074aa83ce438fb26601a4dbe4
Yogeshwaran B <yogiwar33@gmail.com>
2025-05-26 14:37:13 +0530

    Removed the Api exposed api token

--- src/library/weatherapi.ts
+++ src/library/weatherapi.ts
@@ -1,6 +1,6 @@
 // const API_KEY=process.env.WEATHER_API_KEY;
 // console.log(API_KEY)
-const API_KEY = "f595dac20b01966a8d4b585f4e7b5462"
+// const API_KEY = 
 export default async function WeatherFinder (city:string) {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
     const result = await fetch(url);
