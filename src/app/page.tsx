bb9b8da56f8e7137c8b6146643ca0ccce8dbeca7
Yogeshwaran B <yogiwar33@gmail.com>
2025-05-26 12:02:53 +0530

    Updated the API fecth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+"use client"
 import Btn from "@/components/btn"
 import Card from "@/components/card";
-// import WeatherFinder from "@/library/weatherapi";
+import WeatherFinder from "@/library/weatherapi";
+import { useState } from "react";
 export default function Home() {    
+  const[city,setCity]=useState("")
+  const[place,setplace]=useState("");
+  const[temp,setTemp]=useState(0);
   // const API_KEY=process.env.WEATHER_API_KEY;
   // const url=`https://api.openweathermap.org/data/2.5/weather?q=${"chennai"}&appid=${API_KEY}`;
   // async function Weatherfetchapi() {
@@ -19,16 +24,21 @@ export default function Home() {
   // }
 
   // test();
-
+  async function exporting(city: string){
+  const res=await WeatherFinder(city);
+  console.log(res.name)
+    setplace(res.name)
+    setTemp(res.main.temp)
+}
 
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="flex gap-5 m-16">
-        <input type="text" className="w-[1000px] rounded-lg"/>
-        <div><Btn text="Enter"/></div>
+        <input type="text" onChange={(e)=>setCity(e.target.value)} className="w-[1000px] rounded-lg"/>
+        <div><Btn text="Enter" onClick={()=>exporting(city)}/></div>
       </div>
       <div>
-        <Card/>
+        <Card place={place} temp={temp}/>
       </div>
     </div>
   );
--- src/library/weatherapi.ts
+++ src/library/weatherapi.ts
@@ -1,10 +1,13 @@
-const API_KEY=process.env.WEATHER_API_KEY;
+// const API_KEY=process.env.WEATHER_API_KEY;
+// console.log(API_KEY)
+const API_KEY = "f595dac20b01966a8d4b585f4e7b5462"
 export default async function WeatherFinder (city:string) {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
     const result = await fetch(url);
+    // console.log(result)
     if(result.ok){
-        return result.json()
+        return (result.json());
     }else{
-        return "Fetching is not successfull"
+        return ("fecth failed")
     }
 }
